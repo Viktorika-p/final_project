02c0cfb613ba8bdf1cf98486c187736c9473b017
Viktoriia <77969607+Viktorika-p@users.noreply.github.com>
2021-07-05 12:17:58 +0300

    Add files via upload

---
+++ myblog/blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_filter = ('time_create',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
